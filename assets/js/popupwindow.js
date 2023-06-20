9a00a5447fe12c6d4142b194077ad76ca1269649
yusufyusie <yesuffenta023@gmail.com>
2023-06-20 18:21:30 +0300

    Update the popup window java script file to remove syntax error

--- assets/js/popupwindow.js
+++ assets/js/popupwindow.js
@@ -13,7 +13,89 @@ for (let i = 0; i < arraydisplayPopupBtn.length; i++)
       });
     });
   }
-
+  // project details 
+  const dataprojects = [
+    {
+      url: './assets/projects/Snapshoot-Portfolio-desktop.svg',
+      title: 'Project component 1',
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys ',
+      technologies: {tech1: 'HTMl', tech2: 'JS',tech3: 'CSS', tech4: 'Ruby & Rails',
+        },
+      linkVersion: 'https://example.com/liveversion1/',
+      linkSource: 'https://github.com/yusufyusie/work-project-1',
+    },
+    {
+      url: './assets/projects/modal_desktop.png',
+      title: 'Project component 2',
+      description:
+        'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      technologies: {
+        tech1: 'HTMl',
+        tech2: 'JS',
+        tech3: 'CSS',
+        tech4: 'Ruby & Rails',
+      },
+      linkVersion: 'https://example.com/liveversion2/',
+      linkSource: 'https://github.com/yusufyusie/work-project-2',
+    },
+    {
+      url: './assets/projects/Snapshoot-Portfolio-desktop.svg',
+      title: 'Project component 3',
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      technologies: {
+        tech1: 'HTMl',
+        tech2: 'JS',
+        tech3: 'CSS',
+        tech4: 'Ruby & Rails',
+      },
+      linkVersion: 'https://example.com/liveversion3/',
+      linkSource: 'https://github.com/yusufyusie/work-project-3',
+    },
+    {   
+      url: './assets/projects/modal_desktop.png',
+      title: 'Project component 4',
+      description:
+        'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem',
+      technologies: {
+        tech1: 'HTMl',
+        tech2: 'JS',
+        tech3: 'CSS',
+        tech4: 'Ruby & Rails',
+      },
+      linkVersion: 'https://example.com/liveversion4/',
+      linkSource: 'https://github.com/yusufyusie/work-project-4',
+    },
+    {
+      url: './assets/projects/Snapshoot-Portfolio-desktop.svg',
+      title: 'Project component 5',
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. a',
+      technologies: {
+        tech1: 'HTMl',
+        tech2: 'JS',
+        tech3: 'CSS',
+        tech4: 'Ruby & Rails',
+      },
+      linkVersion: 'https://example.com/liveversion5/',
+      linkSource: 'https://github.com/yusufyusie/work-project-5',
+    },
+    {
+      url: './assets/projects/modal_desktop.png',
+      title: 'Project component 6',
+      description:
+        'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+      technologies: {
+        tech1: 'HTMl',
+        tech2: 'JS',
+        tech3: 'CSS',
+        tech4: 'Ruby & Rails',
+      },
+      linkVersion: 'https://example.com/liveversion6/',
+      linkSource: 'https://github.com/yusufyusie/work-project-6',
+    },
+  ];
 
   const cardDetailsMobile = (index) => `
   <article class="card-details-mobile">
@@ -45,6 +127,6 @@ for (let i = 0; i < arraydisplayPopupBtn.length; i++)
                   <img src="./assets/images/github_white.svg" alt="see github icon">
                   </a>
           </div>
-          <script src="projectdetails.js"></script>
+          <script src="popupwindow.js"></script>
   </article>
   `;
