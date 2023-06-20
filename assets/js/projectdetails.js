09f0bbb9695c8816ebe9bfa722c3bc2136357f8b
yusufyusie <yesuffenta023@gmail.com>
2023-06-20 18:14:32 +0300

    Update the javascript linter to to define constant errors

--- assets/js/projectdetails.js
+++ assets/js/projectdetails.js
@@ -81,36 +81,4 @@ const dataprojects = [
     },
   ];
 
-  const cardDetailsMobile = (index) => `
-  <article class="card-details-mobile">
-          <button type="button" class="btn-close-details">
-              <img src="./assets/images/Icon-Cancel.svg" alt="cancel icon">
-          </button>
-          <div class="card-image-details-mobile">
-              <img src=${dataprojects[index].url} alt="a back in mountain">
-          </div>
-          <h2 class="card-title-mobile">
-              ${dataprojects[index].title}
-          </h2>
-          <ul class="tags-mobile">
-              <li class="tag-mobile">${dataprojects[index].technologies.tech1}</li>
-              <li class="tag-mobile">${dataprojects[index].technologies.tech2}</li>
-              <li class="tag-mobile">${dataprojects[index].technologies.tech3}</li>
-              <li class="tag-mobile">${dataprojects[index].technologies.tech4}</li>
-          </ul>
-          <p class="card-paragraph-mobile">
-            ${dataprojects[index].description}
-          </p>
-          <div class="card-button-mobile">
-                  <a href=${dataprojects[index].linkVersion} class="btn-card-details-mobile">
-                  See Live
-                  <img src="./assets/images/see_live_icon.svg" alt="see live icon">
-                  </a>
-                  <a href=${dataprojects[index].linkSource} class="btn-card-details-mobile">
-                  See Sources
-                  <img src="./assets/images/github_white.svg" alt="see github icon">
-                  </a>
-          </div>
-          <script src="popupwindow.js"></script>
-  </article>
-  `;
+  
