0fff0a17f09686e401fe936fa6376a006fa7abd3
yusufyusie <yesuffenta023@gmail.com>
2023-06-22 17:35:41 +0300

    Update the preserve data on local storage

--- assets/js/localstore.js
+++ assets/js/localstore.js
@@ -15,3 +15,50 @@ const screenTest = (e) => {
     }
   };
 
+  
+screenTest(mediaqueryList);
+
+mediaqueryList.addListener(screenTest);
+
+let inputData = {};
+if (localStorage.savedForm) {
+  inputData = JSON.parse(localStorage.getItem('savedForm'));
+}
+
+fullName.addEventListener('change', () => {
+  inputData.fullName = fullName.value;
+});
+
+firstName.addEventListener('change', () => {
+  inputData.firstName = firstName.value;
+});
+
+lastName.addEventListener('change', () => {
+  inputData.lastName = lastName.value;
+});
+
+email.addEventListener('change', () => {
+  inputData.email = email.value;
+});
+
+textArea.addEventListener('change', () => {
+  inputData.textArea = textArea.value;
+});
+
+const fillDataInput = () => {
+  if (inputData.fullName) {
+    fullName.value = inputData.fullName;
+  }
+  if (inputData.firstName) {
+    firstName.value = inputData.firstName;
+  }
+  if (inputData.lastName) {
+    lastName.value = inputData.lastName;
+  }
+  if (inputData.email) {
+    email.value = inputData.email;
+  }
+  if (inputData.textArea) {
+    textArea.value = inputData.textArea;
+  }
+};
