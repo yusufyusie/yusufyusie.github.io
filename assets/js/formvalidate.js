41c5d3e8840c2aa198654f9c76cff12a8a42900d
yusufyusie <yesuffenta023@gmail.com>
2023-06-22 15:07:56 +0300

    Update my website with to validate form fildes

--- assets/js/formvalidate.js
+++ assets/js/formvalidate.js
@@ -1,19 +1,19 @@
-const form = document.getElementById('#contact_form');
-
-form.addEventListener('submit', (e) => {
-    const email = document.getElementById('email').value;
+const form = document.querySelector('.form-input');
+const email = document.getElementById('email');
+form.addEventListener('submit', (event) => {
     const errorMsgElement = document.querySelector('.error_msg');
     const errorMsg = 'Email should be in lower case.';
-    e.preventDefault();
-    const regex = /[A-Z]/;
-    if (!regex.test(email.value)) {
-      form.submit();
-    } else {
-        errorMsgElement.innerHTML = errorMsg;
-    }
+    event.preventDefault();
+    const reg = /[A-Z]/;
+    if (!reg.test(email.value))
+        {
+            form.submit();
+        } else {
+
+            errorMsgElement.innerHTML = errorMsg; 
+        }      
   });
   
-
 
 
 
